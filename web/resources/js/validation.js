949ee4eb4d98ed237793f647a39cd336d6fe3122
김우식 <zlzldntlr1@naver.com>
2022-08-31 20:58:10 +0900

    inspection setting complete

--- web/resources/js/validation.js
+++ web/resources/js/validation.js
@@ -1,34 +1,60 @@
-/**
- * Function Description,
- * Test Function Call,
- * Test Function Call2
- *
- * @param {param1} param1 Parameter Description
- * @param {param2} param2 Parameter Description
- * @return {String} Return Description
- * */
-function test(param1, param2) {
-  return '';
-}
+const loginSubmit = () => {
+  return inspection({
+    selector: '[name="email"]',
+    regex_type: 'email',
+    empty_text: '이메일을 입력해주세요.',
+    failed_text: '이메일을 정확히 입력해주세요.',
+  }) && inspection({
+    selector: '[name="password"]',
+    regex_type: 'password',
+    empty_text: '패스워드를 입력해주세요.',
+    failed_text: '패스워드를 정확히 입력해주세요. (8자 이상 20자 이하, 영문, 숫자, 특수문자(@$!%*#?&^) 모두 포함)',
+  });
+};
 
-/**
- * @function test1
- *
- * @param {param1} param1 Parameter Description
- * @param {param2} param2 Parameter Description
- * @return {String} Return Description
- * */
-function test1(param1, param2) {
-  return '';
-}
+const findIdSubmit = () => {
+  return inspection({
+    selector: '[name="name"]',
+    regex_type: 'name',
+    empty_text: '이름을 입력해주세요.',
+    failed_text: '이름을 정확히 입력해주세요. (2~20자 영어, 한글, 숫자, 띄어쓰기)',
+  }) && inspection({
+    selector: '[name="phone"]',
+    regex_type: 'phone',
+    empty_text: '휴대폰 번호를 입력해주세요.',
+    failed_text: '휴대폰 번호를 정확히 입력해주세요. (3자리, 3~4자리, 4자리, 띄어쓰기나 \'-\' 허용)',
+  });
+};
 
-/**
- * @function test2
- *
- * @param {param1} param1 파라미터 상세
- * @param {param2} param2 파라미터 상세
- * @return {String} Return Description
- * */
-function test2(param1, param2) {
-  return '';
+const findPsSubmit = () => {
+  return inspection({
+    selector: '[name="email"]',
+    regex_type: 'email',
+
+    empty_text: '이메일을 입력해주세요.',
+    failed_text: '이메일을 정확히 입력해주세요.',
+  }) && inspection({
+    selector: '[name="email_token"]',
+    regex_type: 'verify_code',
+    empty_text: '인증 코드를 입력해주세요.',
+    failed_text: '인증 코드를 정확히 입력해주세요. (10자리)',
+  });
+};
+
+const verifyMailSubmit = () => {
+  return inspection({
+    selector: '[name="email"]',
+    regex_type: 'email',
+    empty_text: '이메일을 입력해주세요.',
+    failed_text: '이메일을 정확히 입력해주세요.',
+  });
+};
+
+const verifyPhoneSubmit = () => {
+  return inspection({
+    selector: '[name="phone"]',
+    regex_type: 'phone',
+    empty_text: '휴대폰 번호를 입력해주세요.',
+    failed_text: '휴대폰 번호를 정확히 입력해주세요. (3자리, 3~4자리, 4자리, 띄어쓰기나 \'-\' 허용)',
+  });
 }
